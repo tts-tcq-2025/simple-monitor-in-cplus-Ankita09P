8ef04b67c1431c3b2b6fbce78d8f7d7c782715b1
Ankita09P <panpaliyaankita12@gmail.com>
2025-08-04 16:57:49 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -15,7 +15,7 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
   }
   return true;
 }
-
+ 
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
